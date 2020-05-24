9f5348fa4ef0146685dc785ffdd6f351934497fd
Jordan So <jordan.so21@ycdsbk12.ca>
2020-05-24 04:53:36 -0400

    Pig Latin

--- .py
+++ .py
@@ -1,13 +1,13 @@
 def piglatin(word):
-#Checks if word has vowels" 
-    vowels = "aeiou"    
-    
+    #assuming no numbers
+    vowels = "aeiou"
+
     if word[0] in vowels:
         print(word + "way")
     else:
         for i in range(len(word)):
             if word[0] not in vowels:
                 word = word[1:] + word[0]
+            else:
+                break
         print(word + "ay")
-
-piglatin(str(input()))
