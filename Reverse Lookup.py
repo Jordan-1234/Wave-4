2cbc8a67ec89114ed169fba102bc9f2c829f99d6
Jordan So <jordan.so21@ycdsbk12.ca>
2020-05-26 02:54:22 -0400

    Reverse Lookup

--- Reverse Lookup.py
+++ Reverse Lookup.py
@@ -7,8 +7,8 @@ def reverse_lookup(dictionary, value):
 
 animal_Names = {"John": "Dog", "Jordan": "Dog","Mary": "Lizard", "Caleb": "Rat"}
 #Prints out multiple keys
-print("The names are: ", reverse_lookup(names, "Dog"))
+print("The names are: ", reverse_lookup(animal_Names, "Dog"))
 #Prints out a single keys
-print("The names are: ", reverse_lookup(names, "Lizard"))
+print("The names are: ", reverse_lookup(animal_Names, "Lizard"))
 #Prints out no keys
-print("The names are: ", reverse_lookup(names, "Tiger"))
+print("The names are: ", reverse_lookup(animal_Names, "Tiger"))
